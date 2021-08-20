643ae1cef4b4b63a3e1058ca5ee5c0115157a6f1
mireie <michael.reiersgaard@gmail.com>
2021-08-20 12:34:57 -0700

    updated routes, update seed to remove spaces in urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :shows do 
-    collection do 
-      get 'upcoming'
-      get 'past'
-      get 'next_week'
-      get 'all_ages'
+  resources :venues do
+    resources :shows do
+      collection do
+        get "upcoming"
+        get "past"
+        get "next_week"
+        get "all_ages"
+      end
     end
   end
 end
