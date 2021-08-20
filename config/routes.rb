ceda40eed3996dd29e33ee355eb3347b7fa60509
mireie <michael.reiersgaard@gmail.com>
2021-08-20 13:23:36 -0700

    add api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :venues do
-    resources :shows do
-      collection do
-        get "upcoming"
-        get "past"
-        get "next_week"
-        get "all_ages"
+  namespace :api do
+    namespace :v1 do
+      resources :venues do
+        resources :shows do
+          collection do
+            get "upcoming"
+            get "past"
+            get "next_week"
+            get "all_ages"
+          end
+        end
+      end
+      resources :shows do
+        collection do
+          get "upcoming"
+          get "past"
+          get "next_week"
+          get "all_ages"
+        end
       end
     end
   end
-  resources :shows
 end
