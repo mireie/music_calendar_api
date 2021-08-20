4885cf01722a56bb9f12bc667f6a742fed4e0415
mireie <michael.reiersgaard@gmail.com>
2021-08-20 10:52:35 -0700

    add next week

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,6 @@ class Show < ApplicationRecord
     validates field, presence: true
   end
   scope :upcoming, -> {where("showtime >= ?", DateTime.now)}
-  scope :next_week, -> {where("showtime >= ? && showtime <= ?", DateTime.now, DateTime.now+7)}
+  scope :next_week, -> {upcoming.where("showtime <= ?", DateTime.now+7)}
   scope :past, -> {where("showtime < ?", DateTime.now)}
 end
